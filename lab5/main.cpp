10ebe66fdb8705ae0f70e477fd3f4ae3664b3d58
voinov-arduino <voinovpromo@gmail.com>
2022-11-09 00:03:08 +0500

    Initial Commit

---
+++ lab5/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab5
+//
+//  Created by Дмитрий Вискунов on 08.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
